dd458d15a6dab8eacfe05e4a1236405f7af5c030
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-09-05 15:52:30 +0200

    Added alternative java implementation

--- MatrixMultiplicationJava/src/Program2.java
+++ MatrixMultiplicationJava/src/Program2.java
@@ -1,39 +1,39 @@
-public class Program {
+public class Program2 {
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
         int M = args.length > 1 ? Integer.parseInt(args[1]) : N;
         int K = args.length > 2 ? Integer.parseInt(args[2]) : N;
 
-        double[][] A, B, C;
+        double[] A, B, C;
 
-        A = new double[N][K];
+        A = new double[N * K];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < K; j++) {
-                A[i][j] = 1 + i + j;
+                A[i * N + j] = 1 + i + j;
             }
         }
 
-        B = new double[K][M];
+        B = new double[K * M];
 
         for (int i = 0; i < K; i++) {
             for (int j = 0; j < M; j++) {
-                B[i][j] = 1 + i * j;
+                B[i * K + j] = 1 + i * j;
             }
         }
 
-        C = new double[N][M];
+        C = new double[N * M];
 
         for(int i=0; i< N; i++)
             for(int j=0; j<M; j++)
                 for(int k=0; k<K; k++)
-                    C[i][j] += A[i][k] * B[k][j];
+                    C[i * N + j] += A[i * N + k] * B[k * K + j];
 
         double sum = 0;
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                sum += C[i][j];
+                sum += C[i * N + j];
             }
         }
 
