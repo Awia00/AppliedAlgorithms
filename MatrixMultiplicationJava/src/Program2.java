cbda9fd0d125a6b0cb50f8ff899c761b20128844
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-09-05 17:02:34 +0200

    Threaded java program

--- MatrixMultiplicationJava/src/Program2.java
+++ MatrixMultiplicationJava/src/Program2.java
@@ -1,4 +1,8 @@
+import java.util.concurrent.*;
+
 public class Program2 {
+
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
         int M = args.length > 1 ? Integer.parseInt(args[1]) : N;
@@ -24,10 +28,32 @@ public class Program2 {
 
         C = new double[N * M];
 
-        for(int i=0; i< N; i++)
-            for(int j=0; j<M; j++)
-                for(int k=0; k<K; k++)
-                    C[i * N + j] += A[i * N + k] * B[k * K + j];
+        int cores = Runtime.getRuntime().availableProcessors();
+
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        Future<?>[] tasks = (Future<?>[]) new Future[cores];
+
+	for (int t=0; t<cores; t++) {
+            final int a = t;
+            tasks[t] = es.submit(() -> {
+                for(int i=a*N/cores; i<(a+1)*N/cores; i++) {
+                    for(int j=0; j<M; j++) {
+                        for(int k=0; k<K; k++) {
+                            C[i * N + j] += A[i * N + k] * B[k * K + j];
+                        }
+                    }
+                }
+            });
+        }
+
+        for(int i=0; i<cores;i++) {
+            try {
+                tasks[i].get();
+            } catch (ExecutionException | InterruptedException e) { /*Ignore*/ }
+        }
+
+        es.shutdown();
 
         double sum = 0;
 
@@ -39,5 +65,4 @@ public class Program2 {
 
         System.out.println(sum);
     }
-
 }
