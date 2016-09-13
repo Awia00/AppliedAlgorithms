d6238b297d8593b5e9b104f1cb64ec76edabf8fa
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-09-13 14:57:30 +0200

    FMA instructions

--- MatrixMultiplication/MatrixMultiplicationAVX/Program.c
+++ MatrixMultiplication/MatrixMultiplicationAVX/Program.c
@@ -6,7 +6,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-#include<x86intrin.h>
+#include<immintrin.h>
 
 typedef double myfloat;
 typedef unsigned int myindex;
@@ -20,7 +20,10 @@ myindex rm(int i, int m, int N, int M) {return i*M + m; }
 myindex cm(int i, int m, int N, int M) {return m*N + i; }
 
 void MxMnaive(int N, int M, int K, myfloat *A, myfloat *B, myfloat *C) {
-  const int b = 32; // Should be optimum for a 32K size cache.
+  A = __builtin_assume_aligned(A, 32);
+  B = __builtin_assume_aligned(B, 32);
+  C = __builtin_assume_aligned(C, 32);
+  const int b = 64;
 
 #pragma omp parallel for shared(A,B,C,N,M,K) default(none)
   for(int i0=0; i0<N; i0+=b) {
@@ -28,12 +31,13 @@ void MxMnaive(int N, int M, int K, myfloat *A, myfloat *B, myfloat *C) {
       for(int k0=0; k0<K; k0+=b) {
         for(int i=i0; i< i0 + b; i++) {
           for(int j=j0; j< j0 + b; j++) {
-            __m256d c1 = _mm256_set_pd(0.0,0.0,0.0,0.0);
+            __m256d c1 = _mm256_setzero_pd();
+
             for(int k=k0; k< k0 + b; k+=4) {
-              __m256d a1 =  _mm256_loadu_pd(A + rm(i, k, N, K));
-              __m256d b1 = _mm256_loadu_pd(B + cm(k, j, K, M));
-              __m256d d1 = _mm256_mul_pd(a1, b1);
-              c1 = _mm256_add_pd(d1,c1); 
+              __m256d a1 = _mm256_load_pd(A + rm(i, k, N, K));
+              __m256d b1 = _mm256_load_pd(B + cm(k, j, K, M));
+
+              c1 = _mm256_fmadd_pd(a1, b1, c1);
             }
             double *res = (double*)&c1;
 
@@ -44,36 +48,36 @@ void MxMnaive(int N, int M, int K, myfloat *A, myfloat *B, myfloat *C) {
     }
   }
 }
- 
- 
+
+
 int x = 0;
 int nextPR() {
   x =  (x+234532)*((x>> 5 )+12234);
   return x & 16383;
 }
- 
+
 long int hash(long int a, long int b) { return (a  | a<<27)*(b+2352351);}
 int main(int argc, char **argv){
- 
+
   if( argc != 3 ) {
     printf("Usage: mult N seed\n");
     exit(1);
   }
- 
+
   myindex N = atoi(argv[1]);
   x = atoi(argv[2]);
-   
-  myfloat *A = malloc( 3*N*N*sizeof(myfloat));
+
+  myfloat *A = _mm_malloc(3*N*N*sizeof *A, 32);
   myfloat *B = A + (N*N);
   myfloat *C = B + (N*N);
- 
+
   int i,j;
- 
+
   if( A == NULL || B==NULL || C==NULL ) {
-    printf("Could not allocate memory");
+    printf("Could not allocate memory\n");
     exit(2);
   }
- 
+
   for(int k=0;k<N/2+1;k++) {
     for(i=0; i< N; i++) {
       for(j=0; j<N; j++) {
