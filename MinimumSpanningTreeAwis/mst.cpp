258badfac814cabbbc9fe5c3f30b286d0eb25a92
Anders Wind <awia00@gmail.com>
2016-10-17 11:45:17 +0200

    removed faulty implementation

--- MinimumSpanningTreeAwis/mst.cpp
+++ MinimumSpanningTreeAwis/mst.cpp
@@ -71,55 +71,6 @@ long prim(Graph* G){
     return G->mstToInt(mst, j); 
 }
 
-// long prim(Graph* G){
-//     Edge** mst = new Edge*[G->numVertices];
-//     std::priority_queue<PrimVertex*, std::vector<PrimVertex*>, std::function<bool(PrimVertex*, PrimVertex*)>> pq(PrimVertex::compare);
-    
-//     bool* hasBeenTaken = new bool[G->numVertices];
-//     Edge** cheapestRoute = new Edge*[G->numVertices];
-
-//     long j = 0;
-//     pq.push(new PrimVertex(G->vertexList[j]));
-//     bool notFirstRound = false;
-//     while(!pq.empty())
-//     {
-//         PrimVertex* p = pq.top();
-//         pq.pop();
-//         long id = p->v->id;
-        
-//         if(!hasBeenTaken[id]) 
-//         {
-//             hasBeenTaken[id] = true;
-//             Edge** edges = p->v->vertexEdgeList;
-//             for(long i = 0; i < p->v->totalNumEdges; i++)
-//             {
-//                 Edge* edge = edges[i];
-//                 if (cheapestRoute[edge->v1->id] == NULL || cheapestRoute[edge->v1->id]->weight > edge->weight)
-//                 {
-//                     cheapestRoute[edge->v1->id] = edge;
-//                     PrimVertex* pv = new PrimVertex(edge->v1, edge->weight);
-//                     pq.push(pv);
-//                 }
-//                 if (cheapestRoute[edge->v2->id] == NULL || cheapestRoute[edge->v2->id]->weight > edge->weight)
-//                 {
-//                     cheapestRoute[edge->v2->id] = edge;
-//                     PrimVertex* pv = new PrimVertex(edge->v2, edge->weight);
-//                     pq.push(pv);
-//                 }
-//             }
-
-//             if(notFirstRound)
-//             {
-//                 mst[j++] = cheapestRoute[id];
-//             }
-//             else
-//                 notFirstRound = true;
-            
-//         }
-//     }
-//     return G->mstToInt(mst, j); 
-// }
-
 class DisjointSet{
     public:
         class Node{
