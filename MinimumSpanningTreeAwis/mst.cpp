7301c329aea987774009b42b30933466b18de660
Anders Wind <awia00@gmail.com>
2016-10-19 21:47:04 +0200

    mini optimizations - not enough must look closer at kruskal in array form.

--- MinimumSpanningTreeAwis/mst.cpp
+++ MinimumSpanningTreeAwis/mst.cpp
@@ -8,17 +8,18 @@
 
 #include "graph.h"
 
+using namespace std;
 //#ifndef CODEJUDGE
 class PrimVertex{
     public:
         Vertex* v;
-        long cost;
+        int cost;
         PrimVertex(Vertex* vertex)
         {
             v = vertex;
             cost = 10000;
         }
-        PrimVertex(Vertex* vertex, long aCost)
+        PrimVertex(Vertex* vertex, int aCost)
         {
             v = vertex;
             cost = aCost;
@@ -31,7 +32,7 @@ class PrimVertex{
 
 long prim(Graph* G){
     Edge** mst = new Edge*[G->numVertices];
-    std::priority_queue<Edge*, std::vector<Edge*>, std::function<bool(Edge*, Edge*)>> pq(Edge::compare2);
+    priority_queue<Edge*, vector<Edge*>, function<bool(Edge*, Edge*)>> pq(Edge::compare2);
     
     bool* hasBeenTaken = new bool[G->numVertices];
 
@@ -82,23 +83,23 @@ long prim(Graph* G){
 }
 //#endif
 
+class Node{
+    //Node is potentially a helpful class.  Can be changed or deleted without harm
+    public:
+        Node* parent;
+        int rank;
+        Node(){
+            rank = 0;
+            parent = this;
+        }
+};
+
 class DisjointSet{
     public:
-        class Node{
-            //Node is potentially a helpful class.  Can be changed or deleted without harm
-            public:
-                Node* parent;
-                int rank;
-                Node(){
-                    rank = 0;
-                    parent = this;
-                }
-        };
-
         Node** nodes;
         DisjointSet(Vertex** v, long numVertices) {
             nodes = new Node*[numVertices];
-            for(long i = 0; i<numVertices; i++)
+            for(int i = 0; i<numVertices; i++)
             {
                 long id = v[i]->id;
                 nodes[id] = new Node();
@@ -122,6 +123,21 @@ class DisjointSet{
                 parent2->rank++;
             }
         }
+        void setUnion(Node* parent1, Node* parent2){
+            if(parent1->rank > parent2->rank)
+            {
+                parent1->parent = parent2;
+            }
+            else if(parent1->rank < parent2->rank)
+            {
+                parent2->parent = parent1;
+            }
+            else
+            {
+                parent1->parent = parent2;
+                parent2->rank++;
+            }
+        }
 
         Node* find(long id) {
             Node* first = nodes[id];
@@ -130,6 +146,10 @@ class DisjointSet{
             {
                 prev = prev->parent;
             }
+            
+            if(first->parent == prev)
+                return prev;
+                
             Node* leader = prev;
             prev = first;
             // path compression
@@ -153,20 +173,36 @@ long kruskal(Graph* G){
 
     Edge** edgeList = G->edgeList;
     
-    std::sort(edgeList, edgeList + G->numEdges, Edge::compare);
+    sort(edgeList, edgeList + G->numEdges, Edge::compare);
 
     DisjointSet* ds = new DisjointSet(G->vertexList, G->numVertices);
 
     long j = 0;
-    for(long i = 0; i<G->numEdges; i++)
+    Edge* e;
+    // for(int i = 0; i<G->numEdges; i++)
+    // {
+    //     e = edgeList[i];
+    //     if(!ds->sameSet(e->v1->id, e->v2->id))
+    //     {
+    //         ds->setUnion(e->v1->id, e->v2->id);
+    //         mst[j++] = e;
+    //     }
+    // }
+
+    Node* n1;
+    Node* n2;
+    for(int i = 0; i<G->numEdges; i++)
     {
-        Edge* e = edgeList[i];
-        if(!ds->sameSet(e->v1->id, e->v2->id))
+        e = edgeList[i];
+        n1 = ds->find(e->v1->id);
+        n2 = ds->find(e->v2->id);
+        if(!(n1 == n2))
         {
-            ds->setUnion(e->v1->id, e->v2->id);
+            ds->setUnion(n1, n2);
             mst[j++] = e;
         }
     }
+
     return G->mstToInt(mst, j);
 }
 
@@ -175,22 +211,22 @@ long kruskal(Graph* G){
 int main(int argc, char* argv[]){
     long numVertices, numEdges;
     unsigned int seed;
-    std::string infile = "";
+    string infile = "";
     Graph* G;
 
     if(argc == 4)
     {
         //file name, numVertices, numEdges
-        numVertices = std::atol(argv[2]);
-        numEdges = std::atol(argv[3]);
+        numVertices = atol(argv[2]);
+        numEdges = atol(argv[3]);
         G = new Graph(numVertices,numEdges);
         G->graphFromFile(argv[1], seed);
     } 
     else if(argc == 3)
     {
         //random seed, numVertices
-        seed = std::atol(argv[1]);
-        numVertices = std::atol(argv[2]);
+        seed = atol(argv[1]);
+        numVertices = atol(argv[2]);
         numEdges = numVertices*(numVertices - 1)/2; 
         G = new Graph(numVertices,numEdges);
         G->generateRandomWeights(seed); 
@@ -198,9 +234,9 @@ int main(int argc, char* argv[]){
     else if(argc == 5)
     {
         //random seed, numX, numY, skip probability
-        seed = std::atol(argv[1]);
-        long numX = std::atol(argv[2]);
-        long numY = std::atol(argv[3]);
+        seed = atol(argv[1]);
+        long numX = atol(argv[2]);
+        long numY = atol(argv[3]);
         int skipProb = atoi(argv[4]);
         srand(seed);
         numVertices = numX*numY;
@@ -210,10 +246,10 @@ int main(int argc, char* argv[]){
     }
     else
     {
-        std::cout << "Error: " << argc - 1 << "arguments; should be 2, 3, or 4\n";
+        cout << "Error: " << argc - 1 << "arguments; should be 2, 3, or 4\n";
         return 0;
     }
 
-    std::cout <<  kruskal(G) << std::endl;
-    //std::cout <<  prim(G) << std::endl;
+    //cout <<  kruskal(G) << endl;
+    cout <<  prim(G) << endl;
 }
