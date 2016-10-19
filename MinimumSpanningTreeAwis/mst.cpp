8de2796d3e7f74d910670e4b1070a2f913ff51cc
Anders Wind <awia00@gmail.com>
2016-10-19 23:17:56 +0200

    Created a array version of the disjoint set

--- MinimumSpanningTreeAwis/mst.cpp
+++ MinimumSpanningTreeAwis/mst.cpp
@@ -89,9 +89,88 @@ class Node{
         Node* parent;
         int rank;
         Node(){
-            rank = 0;
             parent = this;
         }
+};
+
+class DisjointSetArray{
+    public:
+        int* parents;
+        int* ranks;
+
+        DisjointSetArray(Vertex** v, int numVertices)
+        {
+            parents = new int[numVertices];
+            ranks = new int[numVertices];
+            for(int i = 0; i<numVertices; i++)
+            {
+                parents[v[i]->id] = v[i]->id;
+                ranks[v[i]->id] = 10000;
+            }
+        }
+        
+        void setUnion(int v1, int v2){
+            int parent1 = find(v1);
+            int parent2 = find(v2);
+            if(ranks[parent1] > ranks[parent2])
+            {
+                parents[parent2] = parent1;
+            }
+            else if(ranks[parent1] < ranks[parent2])
+            {
+                parents[parent1] = parent2;
+            }
+            else
+            {
+                parents[parent1] = parent2;
+                ranks[parent2]++;
+            }
+        }
+
+        void setUnionParents(int parent1, int parent2){
+            if(ranks[parent1] > ranks[parent2])
+            {
+                parents[parent2] = parent1;
+            }
+            else if(ranks[parent1] < ranks[parent2])
+            {
+                parents[parent1] = parent2;
+            }
+            else
+            {
+                parents[parent1] = parent2;
+                ranks[parent2]++;
+            }
+        }
+
+        int find(int id) {
+            int first = parents[id];
+            int prev = first;
+            while(parents[prev] != prev)
+            {
+                prev = parents[prev];
+            }
+            
+            if(parents[first] == prev) // if the lenght was 0 or 1
+                return prev;
+                
+            int leader = prev;
+            prev = first;
+            // path compression
+            while(parents[prev] != prev)
+            {
+                int newParent = parents[prev];
+                parents[prev] = leader;
+                prev = newParent;
+            }
+            return leader;
+        }
+
+        bool sameSet(int v1, int v2)
+        {
+            return find(v1) == find(v2);
+        }
+
 };
 
 class DisjointSet{
@@ -111,11 +190,11 @@ class DisjointSet{
             Node* parent2 = find(v2);
             if(parent1->rank > parent2->rank)
             {
-                parent1->parent = parent2;
+                parent2->parent = parent1;
             }
             else if(parent1->rank < parent2->rank)
             {
-                parent2->parent = parent1;
+                parent1->parent = parent2;
             }
             else
             {
@@ -206,11 +285,49 @@ long kruskal(Graph* G){
     return G->mstToInt(mst, j);
 }
 
+long kruskalArray(Graph* G){
+    Edge** mst = new Edge*[G->numVertices]; 
+
+    Edge** edgeList = G->edgeList;
+    
+    sort(edgeList, edgeList + G->numEdges, Edge::compare);
+
+    DisjointSetArray* ds = new DisjointSetArray(G->vertexList, G->numVertices);
+
+    long j = 0;
+    Edge* e;
+    // for(int i = 0; i<G->numEdges; i++)
+    // {
+    //     e = edgeList[i];
+    //     if(!ds->sameSet(e->v1->id, e->v2->id))
+    //     {
+    //         ds->setUnion(e->v1->id, e->v2->id);
+    //         mst[j++] = e;
+    //     }
+    // }
+
+    int n1;
+    int n2;
+    for(int i = 0; i<G->numEdges; i++)
+    {
+        e = edgeList[i];
+        n1 = ds->find(e->v1->id);
+        n2 = ds->find(e->v2->id);
+        if(!(n1 == n2))
+        {
+            ds->setUnionParents(n1, n2);
+            mst[j++] = e;
+        }
+    }
+
+    return G->mstToInt(mst, j);
+}
+
 
 
 int main(int argc, char* argv[]){
     long numVertices, numEdges;
-    unsigned int seed;
+    unsigned int seed = 0;
     string infile = "";
     Graph* G;
 
@@ -251,5 +368,6 @@ int main(int argc, char* argv[]){
     }
 
     //cout <<  kruskal(G) << endl;
-    cout <<  prim(G) << endl;
+    cout <<  kruskalArray(G) << endl;
+    //cout <<  prim(G) << endl;
 }
