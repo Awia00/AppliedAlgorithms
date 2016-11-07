07f211c3ab95831dae54b880a65180cf488d9e89
Anders Wind <awia00@gmail.com>
2016-11-07 13:46:55 +0100

    Moved rank and parent into a node class instead of using the edges for it - wasting space in prims

--- MinimumSpanningTreeAwis/disjointset.h
+++ MinimumSpanningTreeAwis/disjointset.h
@@ -2,16 +2,31 @@
 
 #define DisjointSetHeader
 #include "graph.h"
+
+struct Node{
+    int rank;
+    Node* parent;
+    Node()
+    {
+        rank = 1;
+        parent = this;
+    }
+};
+
 class DisjointSet{
     public:
-        Vertex** nodes;
+        Node** nodes;
         DisjointSet(Vertex** v, long numVertices) {
-            nodes = v;
+            nodes = new Node*[numVertices];
+            for(int i = 0; i<numVertices; i++)
+            {
+                nodes[i] = new Node();
+            }
         }
 
-        Vertex* find(long id);
+        Node* find(long id);
         void setUnion(long v1, long v2);
-        void setUnion(Vertex* parent1, Vertex* parent2);
+        void setUnion(Node* parent1, Node* parent2);
         bool sameSet(long v1, long v2);
 };
 #endif
