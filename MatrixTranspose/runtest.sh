c394def7d1f6f79092db42f7a785d23f462c8e7e
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-11-08 08:03:27 +0100

    Ran large test on transpose

--- MatrixTranspose/runtest.sh
+++ MatrixTranspose/runtest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for i in {1..16}
+touch testresult.dat
+echo "# This file is called testresult.dat" > testresult.dat
+echo "# Side size\tReal\tUser\tSystem" >> testresult.dat
+for i in {1..16384}
 do
-	VAL=$((2**i))
-	/usr/bin/time --format "%C\t%e\t%U\t%S" -o testresult.txt --append ./tr_sk $VAL $VAL 0
+	/usr/bin/time --format "$i\t%e\t%U\t%S" -o testresult.dat --append ./tr_sk $i $i 0
 done
