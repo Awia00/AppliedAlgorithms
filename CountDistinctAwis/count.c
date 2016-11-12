5e424839f6c424c45e2b5bf24d6a10b9ec87bf8f
Anders Wind <awia00@gmail.com>
2016-11-12 11:26:23 +0100

    Cleaned the code a bit

--- CountDistinctAwis/count.c
+++ CountDistinctAwis/count.c
@@ -5,76 +5,59 @@
 
 const int BIT_AMT = 32;
 const unsigned int A[] = {
-    0x21ae4036,
-    0x32435171,
-    0xac3338cf,
-    0xea97b40c,
-    0x0e504b22,
-    0x9ff9a4ef,
-    0x111d014d,
-    0x934f3787,
-    0x6cd079bf,
-    0x69db5c31,
-    0xdf3c28ed,
-    0x40daf2ad,
-    0x82a5891c,
-    0x4659c7b0,
-    0x73dc0ca8,
-    0xdad3aca2,
-    0x00c74c7e,
-    0x9a2521e2,
-    0xf38eb6aa,
-    0x64711ab6,
-    0x5823150a,
-    0xd13a3a9a,
-    0x30a5aa04,
-    0x0fb9a1da,
-    0xef785119,
-    0xc9f0b067,
-    0x1e7dde42,
-    0xdda4a7b2,
-    0x1a1c2640,
-    0x297c0633,
-    0x744edb48,
-    0x19adce93}; 
+    0x21ae4036,    0x32435171,    0xac3338cf,    0xea97b40c,
+    0x0e504b22,    0x9ff9a4ef,    0x111d014d,    0x934f3787,
+    0x6cd079bf,    0x69db5c31,    0xdf3c28ed,    0x40daf2ad,
+    0x82a5891c,    0x4659c7b0,    0x73dc0ca8,    0xdad3aca2,
+    0x00c74c7e,    0x9a2521e2,    0xf38eb6aa,    0x64711ab6,
+    0x5823150a,    0xd13a3a9a,    0x30a5aa04,    0x0fb9a1da,
+    0xef785119,    0xc9f0b067,    0x1e7dde42,    0xdda4a7b2,
+    0x1a1c2640,    0x297c0633,    0x744edb48,    0x19adce93}; 
 
-#define MAX(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-
-int h(int x)
+// h
+int gf2_hash_opt(int x)
 {
     int r = 0;
     for(int i = 0; i < BIT_AMT; i++)
     {
         int hash = (__builtin_popcount(A[i] & x) & 1);
         r += hash << (BIT_AMT-i-1);
-        if(hash)
+        if(hash) // the first 1 has been found no reason to calculate the rest
             return r;
     }
     return r;
 }
 
-int f(int x) {
+// h
+int gf2_hash(int x)
+{
+    int r = 0;
+    for(int i = 0; i < BIT_AMT; i++)
+    {
+        r += (__builtin_popcount(A[i] & x) & 1) << (BIT_AMT-i-1);
+    }
+    return r;
+}
+
+// f
+int heuristic_hash(int x) { 
     return ((x*0Xbc164501) & 0X7fe00000) >> 21;
 }
 
 double hyperloglog(int m)
 {
     int* M = (int*)calloc(m, sizeof(int));
-    //printf("hey\n");
     int input;
-    int result = scanf("%d", &input);
-    while(result == 1)
+    int exit = scanf("%d", &input);
+    while(exit == 1)
     {
-        int j = f(input);
-        int new = __builtin_clz(h(input))+1;
+        int j = heuristic_hash(input);
+        int new = __builtin_clz(gf2_hash_opt(input)) + 1;
         if(M[j] < new)
         {
             M[j] = new;
         }
-        result = scanf("%d", &input);
+        exit = scanf("%d", &input);
     }
 
     double Z = 0;
@@ -97,7 +80,21 @@ double hyperloglog(int m)
     return E;
 }
 
-int main(int argc, char **argv) {
+int question1()
+{
+    int input;
+    int exit = scanf("%d", &input);
+    while(exit == 1)
+    {
+        int j = gf2_hash(input);
+        printf("%d\n", j);
+        exit = scanf("%d", &input);
+    }
+    return !exit;
+}
+
+int question3()
+{
     int threshold;
     int exit = scanf("%d", &threshold);
     double result = hyperloglog(1024);
@@ -108,15 +105,6 @@ int main(int argc, char **argv) {
     return !exit;
 }
 
-// int main(int argc, char **argv) {
-//     int input;
-//     int result = scanf("%d", &input);
-//     while(result == 1)
-//     {
-//         int j = h(input);
-//         printf("%d\n", j);
-//         result = scanf("%d", &input);
-//     }
-
-//     return 0;
-// }
+int main(int argc, char **argv) {
+    return question3();
+}
