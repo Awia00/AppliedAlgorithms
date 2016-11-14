b124fced9eef111d8363b3886d97244a29be1e68
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-11-14 20:59:22 +0100

    Small speedup by using cin without synchronization

--- CountDistinct/c/hyperloglog.cpp
+++ CountDistinct/c/hyperloglog.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <stdexcept>
 #include "hashlib.h"
 
 using namespace std;
@@ -14,19 +15,23 @@ double hyperLogLog(int m) {
     // end
     int yi, j, count;
 
-    while (scanf("%d", &yi) == 1) {
-        j = f(yi);
-        count = h_first(yi) + 1;
-        if (M[j] < count) {
-            M[j] = count;
+    try {
+        while (cin >> yi) {
+            j = f(yi);
+            count = h_first(yi);
+            if (M[j] < count) {
+                M[j] = count;
+            }
         }
     }
-    
+    catch (const invalid_argument& ia) {
+        // ignore
+    }
 
     // Z := 1/(2^(-M[0])+...+2^(-M[m-1]))
     // V := |{i | M[i]=0|}|
-    double Z = 0;
-    double V = 0;
+    double Z = 0.0;
+    double V = 0.0;
     for (int i = 0; i < m; i++) {
         Z+= (pow(2.0, -M[i]));
         if (M[i] == 0) V++;
@@ -34,10 +39,10 @@ double hyperLogLog(int m) {
     Z = 1/Z;
 
     // E := m*m*Z*0.7213/(1 + 1.079/m)
-    double E = m*m*Z*0.7213/(1 + 1.079/m);
+    double E = m*m*Z*0.7213/(1.0 + 1.079/m);
 
 //        if (E < 2.5*m and V > 0) then E:= m * ln(m/V)
-    if (E < 2.5*m && V > 0) {
+    if (E < 2.5*m && V > 0.0) {
         E = m * log(m/V);
     }
 
@@ -46,18 +51,12 @@ double hyperLogLog(int m) {
 }
 
 int main() {
+    std::ios::sync_with_stdio(false);
     int threshold;
+    cin >> threshold;
     
-    int t = scanf("%d", &threshold);
 
-    if (t != 1) {
-        cout << "wrong input?" << endl;
-        return t;
-    }
-    
-    double result = hyperLogLog(1024);
-
-    if (result < threshold) {
+    if (hyperLogLog(1024) < threshold) {
         cout << "below" << endl;
     } else {
         cout << "above" << endl;
