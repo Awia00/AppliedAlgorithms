5544b9feb3b34cf81e0b0d198efe50c4f6cbc8ef
Anders Wind <awia00@gmail.com>
2016-12-15 00:43:03 +0100

    Added somef of the project, latex structure for the exam

---
+++ Exam-awis/src/cpp/main.cpp
@@ -0,0 +1,15 @@
+#include<queue>
+#include<iostream>
+using namespace std;
+
+class keyvalue{
+    public:
+        int key;
+        int value;
+        keyvalue(int k, int v) : key(k), value(v) {}
+};
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
